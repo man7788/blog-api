313f5048318e2cb17b3b9919672fae4e339cf049
man7788 <jerman1841@gmail.com>
2023-10-07 04:32:29 +0800

    Update user routes

--- api/routes/users.js
+++ api/routes/users.js
@@ -3,6 +3,12 @@ const router = express.Router();
 
 const userController = require("../controllers/userController");
 
-router.post("/", userController.user_sign_up);
+// router.post("/sign-up", userController.user_sign_up);
+
+// Sign-up user on POST
+router.post("/sign-up", userController.sign_up_b);
+
+// Log-in user on POST
+router.post("/log-in", userController.login_b);
 
 module.exports = router;
