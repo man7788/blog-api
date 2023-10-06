e3129b4599391d9c6a8baad07e0776feb5599f42
man7788 <jerman1841@gmail.com>
2023-10-07 04:27:25 +0800

    Update routes

--- api/app.js
+++ api/app.js
@@ -35,7 +35,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/posts", postRouter);
-app.use("/user", userRouter);
+app.use("/users", userRouter);
 
 // // catch 404 and forward to error handler
 // app.use(function (req, res, next) {
