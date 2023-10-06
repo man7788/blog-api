91763ffb845936e30996fa05cc1bcf2e922df16d
man7788 <jerman1841@gmail.com>
2023-10-07 03:08:39 +0800

    Update routes

--- api/app.js
+++ api/app.js
@@ -34,7 +34,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/post", postRouter);
 app.use("/posts", postRouter);
 app.use("/user", userRouter);
 
