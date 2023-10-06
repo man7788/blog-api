424e91ca49f2205dbdc789ae2cd1dabce989da95
man7788 <jerman1841@gmail.com>
2023-10-07 02:46:41 +0800

    Upade routes

--- api/app.js
+++ api/app.js
@@ -7,7 +7,8 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
 const indexRouter = require("./routes/index");
-const postsRouter = require("./routes/posts");
+const postRouter = require("./routes/posts");
+const userRouter = require("./routes/users");
 
 const app = express();
 
@@ -33,22 +34,24 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/posts", postsRouter);
+app.use("/post", postRouter);
+app.use("/posts", postRouter);
+app.use("/user", userRouter);
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+// // catch 404 and forward to error handler
+// app.use(function (req, res, next) {
+//   next(createError(404));
+// });
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+// // error handler
+// app.use(function (err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render("error");
+// });
 
 module.exports = app;
