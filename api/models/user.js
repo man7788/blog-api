831a8391da35558034f1657889ad21aa5ac48490
man7788 <jerman1841@gmail.com>
2023-10-07 04:32:38 +0800

    Update user model

--- api/models/user.js
+++ api/models/user.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
+  nickname: { type: String, required: true, minLength: 1, maxLength: 50 },
   username: { type: String, required: true, minLength: 1, maxLength: 50 },
   password: { type: String, required: true, minLength: 8, maxLength: 200 },
 });
