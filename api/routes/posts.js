6187059eb7e721260d1b3d714b9c504b51c60f84
man7788 <jerman1841@gmail.com>
2023-10-08 03:37:40 +0800

    Add route get post status with token

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,18 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const verifyToken = require("../controllers/verifyToken");
 
 const postController = require("../controllers/postController");
 
 // Get all published posts
 router.get("/", postController.posts);
 
-// Get a specific post
-router.get("/:id", postController.post_detail);
+// Update publication status or blog posts
+router.get("/publish", verifyToken, postController.posts_status);
 
 // Create new blog post
 router.post("/create", postController.create_post);
 
-// Create new commnet for a blog post
+// Get a specific post
+router.get("/:id", postController.post_detail);
+
+// Create new comment for a blog post
 router.post("/:id/comment", postController.create_comment);
 
 module.exports = router;
