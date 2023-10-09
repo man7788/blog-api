2547afbebd1d99e4cf005f05dbef5fb3f0d10870
man7788 <jerman1841@gmail.com>
2023-10-10 03:28:34 +0800

    Update post controller populate

--- api/controllers/postController.js
+++ api/controllers/postController.js
@@ -4,16 +4,24 @@ const Comment = require("../models/comments");
 const asyncHandler = require("express-async-handler");
 const jwt = require("jsonwebtoken");
 
-// Get all published posts on GET
+// Handle all published posts on GET
 exports.posts = asyncHandler(async (req, res, next) => {
-  const posts = await Post.find({ publish: true });
+  const posts = await Post.find({ publish: true }).populate({
+    path: "author",
+    model: "User",
+    select: "username",
+  });
   res.json(posts);
 });
 
-// Get a specific post and comments on GET
+// Handle a specific post and comments on GET
 exports.post_detail = asyncHandler(async (req, res, next) => {
   const [post, comments] = await Promise.all([
-    Post.findById(req.params.id).populate("author", "username"),
+    Post.findById(req.params.id).populate({
+      path: "author",
+      model: "User",
+      select: "username",
+    }),
     Comment.find({ post: req.params.id }),
   ]);
   const response = { post, comments };
