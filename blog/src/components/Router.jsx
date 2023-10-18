c8f007abd4d704baaf7b66d59b52b6c1fd17a9ba
man7788 <jerman1841@gmail.com>
2023-10-19 04:02:07 +0800

    Add new comment route

--- blog/src/components/Router.jsx
+++ blog/src/components/Router.jsx
@@ -2,12 +2,18 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "../App";
 import Post from "./Post";
 import ErrorPage from "./ErrorPage";
+import NewComment from "./NewComment";
 
 const Router = () => {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <App />,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: "/posts/:postId/comment/new",
+      element: <NewComment />,
       errorElement: <ErrorPage />,
     },
     {
