f7d3f3e434a89fe086f175c30d8e26cdab3a09ac
man7788 <jerman1841@gmail.com>
2023-10-19 08:04:38 +0800

    Update new comment

--- blog/src/components/NewComment.jsx
+++ blog/src/components/NewComment.jsx
@@ -9,15 +9,18 @@ const NewComment = () => {
   const [comment, setComment] = useState("");
   const [author, setAuthor] = useState("");
 
-  const [errors, setErrors] = useState(null);
+  const [serverError, setServerError] = useState(false);
+  const [formErrors, setFormErrors] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const onSubmitForm = (e) => {
     e.preventDefault();
+    setLoading(true);
 
     const newComment = { comment, author };
-    // console.log(newComment);
 
     fetch(`http://localhost:3000/posts/${_id}/comment/create`, {
+      mode: "cors",
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -26,20 +29,43 @@ const NewComment = () => {
     })
       .then((response) => {
         if (response.status >= 400) {
+          setServerError(true);
           throw new Error("server error");
         }
         return response.json();
       })
       .then((response) => {
         if (response && response.errors) {
-          setErrors(response.errors);
+          setFormErrors(response.errors);
           throw new Error("form validation error");
         }
         console.log("Success:", response);
         setRedirect(true);
       })
-      .catch((error) => console.error(error));
+      .catch((error) => {
+        if (error && error.message !== "form validation error") {
+          setServerError(error);
+        }
+        console.error(error);
+      })
+      .finally(() => setLoading(false));
   };
+
+  if (serverError) {
+    return (
+      <div>
+        <h1>New Comment</h1> <p>A network error was encountered</p>
+      </div>
+    );
+  }
+
+  if (loading) {
+    return (
+      <div>
+        <h1>New Comment</h1> <p>Loading...</p>
+      </div>
+    );
+  }
 
   return (
     <>
@@ -69,9 +95,9 @@ const NewComment = () => {
           ></input>
           <button type="submit">Submit</button>
         </form>
-        {errors ? (
+        {formErrors ? (
           <ul>
-            {errors.map((error) => (
+            {formErrors.map((error) => (
               <li key={error.msg}>{error.msg}</li>
             ))}
           </ul>
